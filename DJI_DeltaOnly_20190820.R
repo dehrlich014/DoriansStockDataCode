bc2f2157d5850f929e838c5034b64328ac775182
dehrlich014 <67486405+dehrlich014@users.noreply.github.com>
2020-07-01 10:20:02 -0400

    Update DJI_DeltaOnly_20190820.R

--- DJI_DeltaOnly_20190820.R
+++ DJI_DeltaOnly_20190820.R
@@ -1,10 +1,10 @@
 ####This document reads in the performance data of the Dow Industrial from a chosen time interval
-####And computes two statistics, the change in value from a previous close to a current close, or the AbsChange,
-####And the percentage change between a previous close and a current close, or Delta
+####and computes two statistics, the change in value from a previous close to a current close, or the AbsChange,
+####and the percentage change between a previous close and a current close, or Delta.
 
-####Of equal significance, this document is read into by another document, 'NSRGYMerged8_NormClose_REVISED_20200627.R'
+####Of equal significance, this document is read into by another document, 'NSRGYMerged8_NormClose_REVISED_20200627.R'.
 
-#The dataset comes straight from Yahoo Finance
+#The dataset comes straight from Yahoo Finance.
 DJI_Historical <- read.csv("DJI_20010102_20190820.csv")
 
 library(dplyr)
@@ -19,27 +19,27 @@ DJI_N <- length(DJI_Historical[[1]])
 DJI_Historical <- rename(DJI_Historical,"AdjClose" = "Adj.Close")
 
 ####Reformatting the date into something that R can make sense of
-#DJI_Historical$Date <- (as.Date(DJI_Historical$Date, format = "%m/%d/%y"))
+#DJI_Historical$Date <- (as.Date(DJI_Historical$Date, format = "%m/%d/%y")).
 DJI_Historical$Date <- (as.Date(DJI_Historical$Date))
 
 
-####We just don't need these variables at this time
+####We just don't need these variables at this time.
 DJI_Historical$Open <- NULL
 DJI_Historical$High <- NULL
 DJI_Historical$Low <- NULL
-####We will come back to them, for sure
+####We will come back to them, for sure.
 DJI_Historical$Close <- NULL
-#We don't need the close because we are using the AdjClose... always
+#We don't need the close because we are using the AdjClose... always.
 
 
 DJI_Historical$AbsChange <- 0
 DJI_Historical$Delta <- 0
 ####I'll put the new NormClose variable here, and then come back to it
-####After we do all the usual work with the AdjClose
+####after we do all the usual work with the AdjClose.
 
 
-####This is really what we care most about, getting the deltas from the AdjClose's
-####Once we do this, we can use *these* deltas to back into generating the NormClose
+####This is really what we care most about, getting the deltas from the AdjClose's.
+####Once we do this, we can use *these* deltas to back into generating the NormClose.
 for(i in 2:DJI_N){
 	DJI_Historical$AbsChange[i] <- (DJI_Historical$AdjClose[i] - DJI_Historical$AdjClose[i-1])
 	DJI_Historical$Delta[i] <- (DJI_Historical$AbsChange[i]/DJI_Historical$AdjClose[i-1])
