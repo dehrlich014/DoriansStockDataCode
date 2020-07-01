c29817429024572f7ef2187596bc2b8588abf16b
dehrlich014 <67486405+dehrlich014@users.noreply.github.com>
2020-07-01 10:27:43 -0400

    Update IAU_DeltaOnly_20190820.R

--- IAU_DeltaOnly_20190820.R
+++ IAU_DeltaOnly_20190820.R
@@ -1,10 +1,10 @@
 ####This document reads in the performance data of the ishares Gold ETF from a chosen time interval
-####And computes two statistics, the change in value from a previous close to a current close, or the AbsChange,
-####And the percentage change between a previous close and a current close, or Delta
+####and computes two statistics, the change in value from a previous close to a current close, or the AbsChange,
+####and the percentage change between a previous close and a current close, or Delta.
 
-####Of equal significance, this document is read into by another document, 'NSRGYMerged8_NormClose_REVISED_20200627.R'
+####Of equal significance, this document is read into by another document, 'NSRGYMerged8_NormClose_REVISED_20200627.R'.
 
-#The dataset comes straight from Yahoo Finance
+#The dataset comes straight from Yahoo Finance.
 IAU_Historical <- read.csv("IAU_20070411_20190820.csv")
 library(dplyr)
 library(ggplot2)
@@ -18,28 +18,28 @@ IAU_N <- length(IAU_Historical[[1]])
 
 IAU_Historical <- rename(IAU_Historical,"AdjClose" = "Adj.Close")
 
-####Reformatting the date into something that R can make sense of
+####Reformatting the date into something that R can make sense of.
 #IAU_Historical$Date <- (as.Date(IAU_Historical$Date, format = "%m/%d/%y"))
 IAU_Historical$Date <- (as.Date(IAU_Historical$Date))
 
 
-####We just don't need these variables at this time
+####We just don't need these variables at this time.
 IAU_Historical$Open <- NULL
 IAU_Historical$High <- NULL
 IAU_Historical$Low <- NULL
-####We will come back to them, for sure
+####We will come back to them, for sure.
 IAU_Historical$Close <- NULL
-#We don't need the close because we are using the AdjClose... always
+#We don't need the close because we are using the AdjClose... always.
 
 
 IAU_Historical$AbsChange <- 0
 IAU_Historical$Delta <- 0
 ####I'll put the new NormClose variable here, and then come back to it
-####After we do all the usual work with the AdjClose
+####after we do all the usual work with the AdjClose.
 
 
-####This is really what we care most about, getting the deltas from the AdjClose's
-####Once we do this, we can use *these* deltas to back into generating the NormClose
+####This is really what we care most about, getting the deltas from the AdjClose's.
+####Once we do this, we can use *these* deltas to back into generating the NormClose.
 for(i in 2:IAU_N){
 	IAU_Historical$AbsChange[i] <- (IAU_Historical$AdjClose[i] - IAU_Historical$AdjClose[i-1])
 	IAU_Historical$Delta[i] <- (IAU_Historical$AbsChange[i]/IAU_Historical$AdjClose[i-1])
