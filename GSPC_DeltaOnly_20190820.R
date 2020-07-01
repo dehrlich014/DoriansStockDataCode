cd3ea801af8a416446ef981e823c8c76a54e8322
dehrlich014 <67486405+dehrlich014@users.noreply.github.com>
2020-07-01 10:25:16 -0400

    Update GSPC_DeltaOnly_20190820.R

--- GSPC_DeltaOnly_20190820.R
+++ GSPC_DeltaOnly_20190820.R
@@ -1,10 +1,10 @@
 ####This document reads in the performance data of the S&P from a chosen time interval
-####And computes two statistics, the change in value from a previous close to a current close, or the AbsChange,
-####And the percentage change between a previous close and a current close, or Delta
+####and computes two statistics, the change in value from a previous close to a current close, or the AbsChange,
+####and the percentage change between a previous close and a current close, or Delta.
 
-####Of equal significance, this document is read into by another document, 'NSRGYMerged8_NormClose_REVISED_20200627.R'
+####Of equal significance, this document is read into by another document, 'NSRGYMerged8_NormClose_REVISED_20200627.R'.
 
-#The dataset comes straight from Yahoo Finance
+#The dataset comes straight from Yahoo Finance.
 GSPC_Historical <- read.csv("GSPC_20020730_20190820.csv")
 library(dplyr)
 library(ggplot2)
@@ -17,28 +17,28 @@ GSPC_N <- length(GSPC_Historical[[1]])
 
 GSPC_Historical <- rename(GSPC_Historical,"AdjClose" = "Adj.Close")
 
-####Reformatting the date into something that R can make sense of
+####Reformatting the date into something that R can make sense of.
 #GSPC_Historical$Date <- (as.Date(GSPC_Historical$Date, format = "%m/%d/%y"))
 GSPC_Historical$Date <- (as.Date(GSPC_Historical$Date))
 
 
-####We just don't need these variables at this time
+####We just don't need these variables at this time.
 GSPC_Historical$Open <- NULL
 GSPC_Historical$High <- NULL
 GSPC_Historical$Low <- NULL
 ####We will come back to them, for sure
 GSPC_Historical$Close <- NULL
-#We don't need the close because we are using the AdjClose... always
+#We don't need the close because we are using the AdjClose... always.
 
 
 GSPC_Historical$AbsChange <- 0
 GSPC_Historical$Delta <- 0
 ####I'll put the new NormClose variable here, and then come back to it
-####After we do all the usual work with the AdjClose
+####after we do all the usual work with the AdjClose.
 
 
-####This is really what we care most about, getting the deltas from the AdjClose's
-####Once we do this, we can use *these* deltas to back into generating the NormClose
+####This is really what we care most about, getting the deltas from the AdjClose's.
+####Once we do this, we can use *these* deltas to back into generating the NormClose.
 for(i in 2:GSPC_N){
 	GSPC_Historical$AbsChange[i] <- (GSPC_Historical$AdjClose[i] - GSPC_Historical$AdjClose[i-1])
 	GSPC_Historical$Delta[i] <- (GSPC_Historical$AbsChange[i]/GSPC_Historical$AdjClose[i-1])
